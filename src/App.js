24760357859b136c39511e54d491f5d07c10a5f5
achachsalma <jehhfjvfkbf@gmail.com>
2022-09-16 19:06:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import React from 'react';
+//composant Pht
+// fonction Pht()
+const Pht=()=>{
+return <div className='from-control'>
+<label htmlFor='pht'> prix hors taxe</label>
+<input type='number' id='pht' placeholder='saisir votre pht:'></input>
+</div>;
+}
+//composant Tva
+// fonction Tva()
+const Tva=()=>{
+  return <div className='from-control'>
+  <label htmlFor='tva'> taux Tva</label>
+  <input type='number' id='tva' placeholder='saisir votre tva:'></input>
+  </div>;
+  }
+  //composant buttons
+// fonction buttons()
+const Buttons=()=>{
+  return <div className='from-control'>
+  <button>calculer</button>
+  <button>initialiser</button>
+  </div>
+  };
+  //composant res
+// fonction res()
+const Res=()=>{
+  return <div className='from-control'>
+  <label htmlFor='tva'> resultat :</label>
+  <input type='number' id='tva' ></input>
+  </div>;
+  }
+  const Calculpttc=()=>{
+    return <fieldset>
+      <legend>Calcul de pttc</legend>
+      <Pht/>
+<Tva/>
+<Buttons/>
+<Res/>
+    </fieldset>
+  }
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+<Calculpttc/>
+</div>
   );
 }
 
